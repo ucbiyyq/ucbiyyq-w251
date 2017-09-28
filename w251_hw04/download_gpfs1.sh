8a1dda2f1eca6b71969f5045280797514d5cd645
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2017-09-27 19:59:23 -0500

    tweaked setup sh

--- w251_hw04/download_gpfs1.sh
+++ w251_hw04/download_gpfs1.sh
@@ -1,1 +1,1 @@
-wget http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20120701-fi.gz -O /root/data/googlebooks-eng-all-2gram-20120701-fi.gz
+ssh gpfs1 'wget http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20120701-fi.gz -O /root/data/googlebooks-eng-all-2gram-20120701-fi.gz'
