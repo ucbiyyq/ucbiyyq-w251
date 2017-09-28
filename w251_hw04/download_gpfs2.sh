08b1e72a5863fcb13ce4874983b41ae667bc5def
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2017-09-28 00:20:26 -0400

    tweaked the other download scripts

--- w251_hw04/download_gpfs2.sh
+++ w251_hw04/download_gpfs2.sh
@@ -1,13 +1,10 @@
 url_prefix=http://storage.googleapis.com/books/ngrams/books/
-temp_prefix=/root/temp/
 file_prefix=googlebooks-eng-all-2gram-20090715-
 zip_suffix=.csv.zip
-file_suffix=.csv
-gpfs_prefix=/gpfs/gpfsfpo/gpfs2/
+gpfs_path=/gpfs/gpfsfpo/gpfs2/
 
-for i in {33..35}; do
-    url=${url_prefix}${file_prefix}${i}${zip_suffix}
-    file=${temp_prefix}${file_prefix}${i}${zip_suffix}
-    wget $url -O $file
-    unzip -o $file -d $gpfs_prefix
-done
+
+pushd ${gpfs_path}
+curl ${url_prefix}${file_prefix}[33-35]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
+unzip -o "*.zip"
+popd
