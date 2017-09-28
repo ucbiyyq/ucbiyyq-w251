ade567487d879d80de50ca9873c2a67d7cdb2593
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2017-09-27 23:17:49 -0500

    optimized download gpfs1

--- w251_hw04/download_gpfs1.sh
+++ w251_hw04/download_gpfs1.sh
@@ -1,13 +1,10 @@
 url_prefix=http://storage.googleapis.com/books/ngrams/books/
-temp_prefix=/root/temp/
 file_prefix=googlebooks-eng-all-2gram-20090715-
 zip_suffix=.csv.zip
-file_suffix=.csv
-gpfs_prefix=/gpfs/gpfsfpo/gpfs1/
+gpfs_path=/gpfs/gpfsfpo/gpfs1/
 
-for i in {0..2}; do
-    url=${url_prefix}${file_prefix}${i}${zip_suffix}
-    file=${temp_prefix}${file_prefix}${i}${zip_suffix}
-    wget $url -O $file
-    unzip -o $file -d $gpfs_prefix
-done
+
+pushd ${gpfs_path}
+curl ${url_prefix}${file_prefix}[0-2]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
+unzip -o "*.zip"
+popd
