501a115d8ce84f2de25d59fd91413dd77045616d
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2017-09-28 00:48:00 -0500

    modified the download scripts

--- w251_hw04/download_gpfs2.sh
+++ w251_hw04/download_gpfs2.sh
@@ -5,6 +5,6 @@ gpfs_path=/gpfs/gpfsfpo/gpfs2/
 
 
 pushd ${gpfs_path}
-curl ${url_prefix}${file_prefix}[33-35]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
+curl ${url_prefix}${file_prefix}[33-65]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
 unzip -o "*.zip"
 popd
--- w251_hw04/download_gpfs3.sh
+++ w251_hw04/download_gpfs3.sh
@@ -5,6 +5,6 @@ gpfs_path=/gpfs/gpfsfpo/gpfs3/
 
 
 pushd ${gpfs_path}
-curl ${url_prefix}${file_prefix}[66-68]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
+curl ${url_prefix}${file_prefix}[66-99]${zip_suffix} -o ${file_prefix}#1${zip_suffix}
 unzip -o "*.zip"
 popd
